2363f843e1c5bbfe11555e7519fcd1956ad033f2
RubberLanding <nico.schumann@tutanota.com>
2026-02-02 21:53:29 +0100

    Format the benchmarks. Add block size as arg for Triton.

--- src/nbody/benchmark/benchmark.py
+++ src/nbody/benchmark/benchmark.py
@@ -1,4 +1,3 @@
-import time
 import math
 import argparse  
 import numba
@@ -19,7 +18,7 @@ EPSILON = 1e-4
 WARUM_UP_ITER = 5
 
 def measure_time_torch(pos_host, vel_host, mass_host, dt=0.01, steps=10, compute_forces_func=compute_forces_pytorch_naive):
-    # --- 1. SETUP & TRANSFER ---
+    # --- SETUP & TRANSFER ---
     assert torch.cuda.is_available(), "CUDA is not available!"
     device = torch.device("cuda")   
     print(f"Running on GPU (PyTorch). N={pos_host.shape[0]}, Steps={steps}")
@@ -30,13 +29,13 @@ def measure_time_torch(pos_host, vel_host, mass_host, dt=0.01, steps=10, compute
     mass = torch.tensor(mass_host, device=device, dtype=torch.float32)
     N    = pos.shape[0]
 
-    # --- 2. CONSTANTS PREP ---
+    # --- CONSTANTS PREP ---
     dt_tensor = torch.tensor(dt, device=device, dtype=torch.float32)
     dt2_half  = 0.5 * dt_tensor * dt_tensor
     dt_half   = 0.5 * dt_tensor
     inv_m     = 1.0 / mass.unsqueeze(1)
 
-    # Warm-Up
+    # --- WARM-UP ---
     with torch.no_grad():
         # Initial force calculation
         force_old = compute_forces_func(pos, mass, G, EPSILON).clone()
@@ -50,8 +49,9 @@ def measure_time_torch(pos_host, vel_host, mass_host, dt=0.01, steps=10, compute
     start_event = torch.cuda.Event(enable_timing=True)
     end_event   = torch.cuda.Event(enable_timing=True)
     start_event.record()
+
+    # --- START SIMULATION ---
     with torch.no_grad():
-        # ==================== CORE LOOP ====================
         for step in range(steps):
             
             # [Step A] Update Position
@@ -70,7 +70,7 @@ def measure_time_torch(pos_host, vel_host, mass_host, dt=0.01, steps=10, compute
     end_event.record()
     torch.cuda.synchronize()
     
-    # --- 4. FINALIZE ---
+    # --- FINALIZE ---
     total_time = start_event.elapsed_time(end_event) / 1000.0
     steps_per_second = steps / total_time
     interactions_per_second = steps * N * N / total_time
@@ -78,8 +78,8 @@ def measure_time_torch(pos_host, vel_host, mass_host, dt=0.01, steps=10, compute
 
     return steps, total_time, steps_per_second, interactions_per_second
 
-def measure_time_cupy(pos_host, vel_host, mass_host, dt=0.01, steps=10, compute_forces_func=compute_forces_cupy_naive):
-    # --- 1. SETUP & TRANSFER ---
+def measure_time_cupy(pos_host, vel_host, mass_host, dt=0.01, steps=10, compute_forces_func=compute_forces_cupy_naive, threads=128):
+    # --- SETUP & TRANSFER ---
     print(f"Running on GPU (CuPy). N={pos_host.shape[0]}, Steps={steps}")
     print(f"Using Force Function: {compute_forces_func.__name__}")
     
@@ -91,41 +91,38 @@ def measure_time_cupy(pos_host, vel_host, mass_host, dt=0.01, steps=10, compute_
     force_old = cp.zeros((N, 3), dtype=cp.float32)
     force_new = cp.zeros((N, 3), dtype=cp.float32)
 
-    # --- 2. CONSTANTS PREP ---
+    # --- CONSTANTS PREP ---
     dt_vec   = cp.float32(dt)
     dt2_half = 0.5 * dt_vec * dt_vec
     dt_half  = 0.5 * dt_vec
     inv_m    = 1.0 / mass[:, None]
 
-    threads = 128
     blocks  = (N + threads - 1) // threads
     grid_cfg, block_cfg = (blocks,), (threads,)
 
-    # --- 3. WARM-UP ---
-    # Note: RawKernel arguments require explicit casting (e.g. np.int32, np.float32)
+    # --- WARM-UP ---
+    # Initial force calculation
     compute_forces_func(grid_cfg, block_cfg, (pos, mass, force_old, np.int32(N), np.float32(G), np.float32(EPSILON)))
     
-    # Warm-up loop to stabilize GPU clock
+    # Warm-up loop
     for step in range(WARUM_UP_ITER):
         pos += (vel * dt_vec) + (force_old * inv_m * dt2_half)
         compute_forces_func(grid_cfg, block_cfg, (pos, mass, force_new, np.int32(N), np.float32(G), np.float32(EPSILON)))
         vel += (force_old + force_new) * inv_m * dt_half
         force_old, force_new = force_new, force_old
 
-    # --- 4. START SIMULATION ---
     start_event = cp.cuda.Event()
     end_event   = cp.cuda.Event()
-
     start_event.record()
-    
-    # ==================== CORE LOOP ====================
+
+    # --- START SIMULATION ---
     for step in range(steps):
 
         # [Step A] Update Position
         pos += (vel * dt_vec) + (force_old * inv_m * dt2_half)
 
         # [Step B] Compute Forces
-        compute_forces_func(grid_cfg, block_cfg, (pos, mass, force_new, np.int32(N), np.float32(G), np.float32(EPS)))
+        compute_forces_func(grid_cfg, block_cfg, (pos, mass, force_new, np.int32(N), np.float32(G), np.float32(EPSILON)))
 
         # [Step C] Update Velocity
         vel += (force_old + force_new) * inv_m * dt_half
@@ -137,7 +134,7 @@ def measure_time_cupy(pos_host, vel_host, mass_host, dt=0.01, steps=10, compute_
     end_event.record()
     end_event.synchronize()
     
-    # --- 5. FINALIZE ---
+    # --- FINALIZE ---
     total_time = cp.cuda.get_elapsed_time(start_event, end_event) / 1000.0
     steps_per_second = steps / total_time
     interactions_per_second = steps * N * N / total_time
@@ -145,89 +142,125 @@ def measure_time_cupy(pos_host, vel_host, mass_host, dt=0.01, steps=10, compute_
 
     return steps, total_time, steps_per_second, interactions_per_second
 
-def measure_time_numba(pos_host, vel_host, masses_host, dt=0.01, steps=10, force_func=compute_forces_numba_tiled, threads_per_block=None):
-    N = pos_host.shape[0]    
+def measure_time_numba(pos_host, vel_host, mass_host, dt=0.01, steps=10, compute_forces_func=compute_forces_numba_naive, threads=128):
+    # --- SETUP & TRANSFER ---
+    N = pos_host.shape[0]
     print(f"Running on GPU (Numba). N={N}, Steps={steps}")
-    print(f"Using Force Function: {force_func.__name__}")
+    print(f"Using Force Function: {compute_forces_func.__name__}")
 
-    blocks = math.ceil(N / threads_per_block)
+    pos  = numba.cuda.to_device(pos_host)
+    vel  = numba.cuda.to_device(vel_host)
+    mass = numba.cuda.to_device(mass_host)
+    force_old = numba.cuda.device_array((N, 3), dtype=np.float32)
+    force_new = numba.cuda.device_array((N, 3), dtype=np.float32)
 
-    d_pos = numba.cuda.to_device(pos_host)
-    d_vel = numba.cuda.to_device(vel_host)
-    d_mass = numba.cuda.to_device(masses_host)
-    d_F_old = numba.cuda.device_array((N, 3), dtype=np.float32)
-    d_F_new = numba.cuda.device_array((N, 3), dtype=np.float32)
-    
-    force_func[blocks, threads_per_block](d_pos, d_mass, d_F_old, G, EPSILON)
+    # --- CONSTANTS PREP ---
+    blocks  = math.ceil(N / threads)
 
-    # Warm-Up 
+    if compute_forces_func == compute_forces_numba_tiled: 
+        compute_forces_func = compute_forces_func(threads)
+
+    # --- WARM-UP ---
+    # Initial force calculation
+    compute_forces_func[blocks, threads](pos, mass, force_old, G, EPSILON)
     for step in range(WARUM_UP_ITER):
-        gpu_step_pos[blocks, threads_per_block](d_pos, d_vel, d_mass, d_F_old, dt)
-        force_func[blocks, threads_per_block](d_pos, d_mass, d_F_new, G, EPSILON)
-        gpu_step_vel[blocks, threads_per_block](d_vel, d_mass, d_F_old, d_F_new, dt)
-        d_F_old, d_F_new = d_F_new, d_F_old
-    
-    # Timing
-    start_event = numba.cuda.event()
-    end_event = numba.cuda.event()
+        gpu_step_pos[blocks, threads](pos, vel, mass, force_old, dt)
+        compute_forces_func[blocks, threads](pos, mass, force_new, G, EPSILON)
+        gpu_step_vel[blocks, threads](vel, mass, force_old, force_new, dt)
+        force_old, force_new = force_new, force_old
 
+    start_event = numba.cuda.event()
+    end_event   = numba.cuda.event()
     start_event.record()
+
+    # --- START SIMULATION ---
     for step in range(steps):
-        gpu_step_pos[blocks, threads_per_block](d_pos, d_vel, d_mass, d_F_old, dt)
-        force_func[blocks, threads_per_block](d_pos, d_mass, d_F_new, G, EPSILON)
-        gpu_step_vel[blocks, threads_per_block](d_vel, d_mass, d_F_old, d_F_new, dt)
-        d_F_old, d_F_new = d_F_new, d_F_old
+
+        # [Step A] Update Position: r(t+dt) = r(t) + v(t)dt + 0.5*a(t)dt^2
+        gpu_step_pos[blocks, threads](pos, vel, mass, force_old, dt)
+
+        # [Step B] Compute Forces: F(t+dt)
+        compute_forces_func[blocks, threads](pos, mass, force_new, G, EPSILON)
+
+        # [Step C] Update Velocity: v(t+dt) = v(t) + 0.5*(a(t) + a(t+dt))dt
+        gpu_step_vel[blocks, threads](vel, mass, force_old, force_new, dt)
+
+        # [Step D] Swap References
+        force_old, force_new = force_new, force_old
+    # ===================================================
 
     end_event.record()
     end_event.synchronize()
-    # Elapsed_time returns ms, convert to seconds
+    
+    # --- FINALIZE ---
     total_time = numba.cuda.event_elapsed_time(start_event, end_event) / 1000.0
-
     steps_per_second = steps / total_time
-    interactions_per_second = steps * N * N  / total_time
+    interactions_per_second = steps * N * N / total_time
     print_results(total_time, steps_per_second, interactions_per_second, N)
 
     return steps, total_time, steps_per_second, interactions_per_second
 
-def measure_time_triton(pos_host, vel_host, mass_host, dt=0.01, steps=10, compute_forces_func=compute_accel_triton_naive, block_size=128, ):
-    device = torch.device("cuda")
+def measure_time_triton(pos_host, vel_host, mass_host, dt=0.01, steps=10, compute_forces_func=compute_accel_triton_naive, block_size=32):
+    # --- SETUP & TRANSFER ---
+    assert torch.cuda.is_available(), "CUDA is not available!"
+    device = torch.device("cuda")   
     N = pos_host.shape[0]
+    print(f"Running on GPU (Triton). N={N}, Steps={steps}")
+    print(f"Using Force Function: {compute_forces_func.__name__}")
+
+    pos  = torch.tensor(pos_host,  device=device, dtype=torch.float32)
+    vel  = torch.tensor(vel_host,  device=device, dtype=torch.float32)
+    mass = torch.tensor(mass_host, device=device, dtype=torch.float32)
+    force_old = torch.empty_like(pos)
+    force_new = torch.empty_like(pos)
+
+    # --- CONSTANTS PREP ---
+    dt_vec   = torch.tensor(dt, device=device, dtype=torch.float32)
+    dt2_half = 0.5 * dt_vec * dt_vec
+    dt_half  = 0.5 * dt_vec
+    
     grid = (triton.cdiv(N, block_size),)
 
-    pos = torch.from_numpy(pos_host).to(device)
-    vel = torch.from_numpy(vel_host).to(device)
-    mass = torch.from_numpy(mass_host).to(device)
-    accel_old = torch.empty_like(pos)
-    accel_new = torch.empty_like(pos)
+    # --- WARM-UP ---
+    # WARNING: Although named 'force_old/new' and 'compute_forces_func' to match other backends,
+    # the Triton kernel calculates ACCELERATION directly (Force / Mass).
+    # Therefore, we DO NOT multiply by inv_m in the update steps below.
 
-    dt2_half = 0.5 * dt * dt
-    dt_half = 0.5 * dt
+    # Initial force calculation 
+    compute_forces_func[grid](pos, mass, force_old, G, EPSILON, N, BLOCK_SIZE=block_size)
 
-    compute_forces_func[grid](pos, mass, accel_old, G, EPSILON, N, BLOCK_SIZE=block_size)
-
-    # Warm-Up
     for step in range(WARUM_UP_ITER):
-        pos += (vel * dt) + (accel_old * dt2_half)
-        compute_forces_func[grid](pos, mass, accel_new, G, EPSILON, N, BLOCK_SIZE=block_size)
-        vel += (accel_old + accel_new) * dt_half
-        accel_old = accel_new
+        pos += (vel * dt_vec) + (force_old * dt2_half) 
+        compute_forces_func[grid](pos, mass, force_new, G, EPSILON, N, BLOCK_SIZE=block_size)
+        vel += (force_old + force_new) * dt_half
+        force_old, force_new = force_new, force_old
 
-    # Timing
     start_event = torch.cuda.Event(enable_timing=True)
-    end_event = torch.cuda.Event(enable_timing=True)
+    end_event   = torch.cuda.Event(enable_timing=True)
+    
     start_event.record()
 
+    # --- START SIMULATION ---
     for step in range(steps):
-        pos += (vel * dt) + (accel_old * dt2_half)
-        compute_forces_func[grid](pos, mass, accel_new, G, EPS, N, BLOCK_SIZE=block_size)
-        vel += (accel_old + accel_new) * dt_half
-        accel_old = accel_new
+
+        # [Step A] Update Position: r(t+dt) = r(t) + v(t)dt + 0.5*a(t)dt^2
+        pos += (vel * dt_vec) + (force_old * dt2_half)
+
+        # [Step B] Compute Acceleration: a(t+dt)
+        compute_forces_func[grid](pos, mass, force_new, G, EPSILON, N, BLOCK_SIZE=block_size)
+
+        # [Step C] Update Velocity: v(t+dt) = v(t) + 0.5*(a(t) + a(t+dt))dt
+        vel += (force_old + force_new) * dt_half
+
+        # [Step D] Swap References
+        force_old, force_new = force_new, force_old
+    # ===================================================
 
     end_event.record()
     torch.cuda.synchronize()
-    # Elapsed_time returns ms, convert to seconds
+    
+    # --- FINALIZE ---
     total_time = start_event.elapsed_time(end_event) / 1000.0
-
     steps_per_second = steps / total_time
     interactions_per_second = steps * N * N / total_time
     print_results(total_time, steps_per_second, interactions_per_second, N)
@@ -249,6 +282,7 @@ if __name__== "__main__":
     parser.add_argument("--store-results", action="store_true", help="Store the results.")
     parser.add_argument("--store-plot", action="store_true", help="Store the performance plot.") 
     parser.add_argument("--tpb-numba", type=int, default=128, help="Threads per block for Numba. Should be a multiple of 32.")
+    parser.add_argument("--bs-triton", type=int, default=32, help="Block size for Triton. Should be a multiple of 16.")
 
     args = parser.parse_args()
 
@@ -304,12 +338,18 @@ if __name__== "__main__":
         print(f"Measure {force_func_str.capitalize()}...")
 
         force_func = config["kernels"][force_func_str]
+
+        # Framework-specific arguments
+        measure_kwargs = {}
+        if framework == "triton":
+            measure_kwargs["block_size"] = args.bs_triton
+
         np.random.seed(42) 
         pos = np.random.rand(args.num_bodies, 3).astype(np.float32) * 100.0
         vel = np.random.rand(args.num_bodies, 3).astype(np.float32) - 0.5
         mass = np.random.rand(args.num_bodies).astype(np.float32) * 1e4
 
-        res = config["measure"](pos, vel, mass, dt=args.dt, steps=args.steps, compute_forces_func=force_func)
+        res = config["measure"](pos, vel, mass, dt=args.dt, steps=args.steps, compute_forces_func=force_func, **measure_kwargs)
 
         # Cleanup GPU memory between different framework runs
         cleanup_gpu()
